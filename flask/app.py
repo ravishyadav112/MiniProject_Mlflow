605de3855649faef8d884f6ba12ce7b71ad18493
ravishyadav112 <ravishkumar1242@gmail.com>
2025-08-26 20:32:32 +0530

    Adding flask file and interact with my model and predicting the answer

---
+++ flask/app.py
@@ -0,0 +1,76 @@
+from flask import Flask , render_template , redirect,request , url_for , session
+import mlflow
+import pickle
+from preprocessing_utility import normalize_text
+import os
+import pandas as pd
+
+# Get token from environment
+token = os.getenv("DAGSHUB_PAT")
+if not token:
+    raise EnvironmentError("❌ DAGSHUB_PAT not set")
+
+repo_owner = "ravishyadav112"
+repo_name = "MiniProject_Mlflow"
+
+# ✅ Correct MLflow tracking URI
+mlflow.set_tracking_uri(
+    f"https://{repo_owner}:{token}@dagshub.com/{repo_owner}/{repo_name}.mlflow"
+)
+
+
+##load the model 
+
+def get_latest_model_version(model_name):
+    client = mlflow.MlflowClient()
+    latest_version = client.get_latest_versions(model_name, stages=["Production"])
+    if not latest_version:
+        latest_version = client.get_latest_versions(model_name, stages=["None"])
+    return latest_version[0].version if latest_version else None
+
+model_name = "models"
+model_version = get_latest_model_version(model_name)
+
+model_uri = f'models:/{model_name}/{model_version}'
+model = mlflow.pyfunc.load_model(model_uri)
+
+vectorizer = pickle.load(open('models/vectorizer.pkl','rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def dashboard():
+    result = session.pop("result", None)   # ✅ clear after showing once
+    text = session.pop("text", "")         # ✅ clear text after refresh
+    return render_template("index.html", result=result, text=text)
+
+@app.route('/predict' , methods = ["POST"])
+def analyse():
+    
+
+    ##clean the data
+    text = request.form['text']
+
+    # clean
+    text = normalize_text(text)
+
+    # Vectorise the data  >> bow
+    features = vectorizer.transform([text])
+    features_df = pd.DataFrame.sparse.from_spmatrix(features)
+    features_df = pd.DataFrame(features.toarray(), columns=[str(i) for i in range(features.shape[1])])
+
+
+    ##predict the data
+    result = model.predict(features_df)
+
+    # ✅ store in session
+    session["result"] = int(result[0])
+    session["text"] = text
+
+   
+    return redirect(url_for("dashboard"))
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
